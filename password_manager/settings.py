081af31158bad6e1186212ba82666e9c8d42d12a
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2024-02-02 14:58:40 +0530

    allow render host

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '20d9o51=0l^r&^+404jb!t_8+c(t*_&)!er$x(_8806gr&fxgg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['vault-pass.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['easypass-vault.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'password_manager.urls'
